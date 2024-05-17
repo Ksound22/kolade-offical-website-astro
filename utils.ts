e5d3c532228b067ea5e570ae62cb5336e4e07c39
kolade <chrisjay967@gmail.com>
2024-05-17 21:43:11 +0100

    feat: implemented table of content for each article and removed color from page titles

--- utils.ts
+++ utils.ts
@@ -8,4 +8,20 @@ function formatDate(date: Date): string {
   return new Date(date).toLocaleDateString(undefined, options);
 }
 
-export { formatDate };
+function buildTOC(headings: any) {
+  const toc = [];
+  const parentHeadings = new Map();
+  headings.forEach((h: any) => {
+    const heading = { ...h, subheadings: [] };
+    parentHeadings.set(heading.depth, heading);
+
+    if (heading.depth === 2) {
+      toc.push(heading);
+    } else {
+      parentHeadings.get(heading.depth - 1).subheadings.push(heading);
+    }
+  });
+  return toc;
+}
+
+export { formatDate, buildTOC };
