23ec55e80b420d0957001c34380cd9c8b1a18d5a
kolade <chrisjay967@gmail.com>
2024-05-26 20:37:37 +0100

    feat: implemented pagination

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import astroExpressiveCode from 'astro-expressive-code';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    astroExpressiveCode({
+      plugins: [pluginLineNumbers()],
+      themes: ['monokai'],
+      shiki: {
+        // You can pass additional plugin options here,
+        // e.g. to load custom language grammars:
+      },
+    }),
+  ],
 });
