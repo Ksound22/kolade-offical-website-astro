1368b5b94bf2a03c433f321c118bd176524e4758
kolade <chrisjay967@gmail.com>
2024-06-01 09:46:09 +0100

    feat: added sitemap with astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
+
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 import astroExpressiveCode from 'astro-expressive-code';
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://localhost:4321',
   integrations: [
+    sitemap(),
     react(),
     astroExpressiveCode({
       plugins: [pluginLineNumbers()],
